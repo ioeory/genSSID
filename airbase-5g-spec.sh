bdd13ee684c4766fefc85f5ce4fac2a4b519f1f9
niayigoji <ioeory@gmail.com>
2017-11-20 11:13:00 +0800

    Update airbase-5g-spec.sh

--- airbase-5g-spec.sh
+++ airbase-5g-spec.sh
@@ -1,50 +1,56 @@
 #!/bin/bash
 maclist=$1
 device=$2
-essid=0
-spoof(){
-#	airbase-ng  -e 1111111111111 -c 40  -w "1234567890"  $device &
+location=$(basename $1 |grep -oE ^.*[\.]?)
 
-	for mac in $(grep  -E ^[^#] $maclist)
-	do
-		let i=i+1
-		let essid=essid+111 
-		sleep 1 
-		clear
-		airbase-ng -e $essid -c 36 -a "$mac" $device & 
-		echo "Starting $location$i: $essid..."
-	done
+spoof(){
+#       airbase-ng  -e 1111111111111 -c 40  -w "1234567890"  $device &
+
+        for mac in $(grep  -E ^[^#] $maclist)
+        do
+                let i=i+1
+                #let essid=essid+111
+                essid=$(cat /dev/urandom | head -n 10 | md5sum | head -c 11)
+                sleep 1
+                clear
+                airbase-ng -e $essid -c 36 -a "$mac" $device &
+                echo "Starting $location-$i"
+                echo "SSID:$essid... "
+                echo "MAC:$mac"
+        done
 
 }
 
+if [ $UID -ne 0 ];then
+        echo "This Script Must Run As Root..."
+        exit
+fi
+
+pkill airbase-ng
+
 if [ "$1" = "" ];then
-	echo "no input file"
-	exit 
+        echo "no input file"
+        exit
 elif [ ! -e $1 ];then
-	echo "no such file"
-	exit
+        echo "no such file"
+        exit
 elif [ ! -f $1 ];then
-	echo "invalid file format"
-	exit
+        echo "invalid file format"
+        exit
 fi
 
 if [ "$2" = "" ];then
-	echo "no device input"
-	exit
+        echo "no device input"
+#       device="wlan0"
+#       echo "try using $device.."
+        exit
 else
-	ifconfig  -a |grep $device 
-	if [ $? != 0 ];then
-		exit
-	fi
+        ifconfig  -a |grep $device
+        if [ $? != 0 ];then
+                echo "device not found.."
+                exit
+        fi
 fi
 
-
-#airbase-ng  -e 1111111111111 -c 40  -w "1234567890" $device &
-#sleep 3
-#ifconfig at0 192.168.2.1/24 up &
-pkill airbase-ng
 spoof
-
-
-
-#[-W 1 (for wep)/ -W 1 -z 2 (for wpa)/ -W 1 -Z 2 (for wpa2)]
+sleep 1
