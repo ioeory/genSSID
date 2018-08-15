4c686c3626c5eacc5935898d7c517e53247bd4f4
ioeory <ioeory@gmail.com>
2018-08-15 10:57:45 +0800

    recover files

--- _airbase-5g.sh
+++ _airbase-5g.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD:_airbase-5g.sh
 maclist=$2
 device=$1
 location=$(basename $2 |grep -oE ^.*[\.]?)
@@ -18,32 +17,10 @@ spoof(){
 	        echo "SSID:-- $essid "
 		echo "MAC:--- $mac"
 	done
-=======
-maclist=$1
-device=$2
-location=$(basename $1 |grep -oE ^.*[\.]?)
-
-spoof(){
-#       airbase-ng  -e 1111111111111 -c 40  -w "1234567890"  $device &
-
-        for mac in $(grep  -E ^[^#] $maclist)
-        do
-                let i=i+1
-                #let essid=essid+111
-                essid=$(cat /dev/urandom | head -n 10 | md5sum | head -c 11)
-                sleep 1
-                clear
-                airbase-ng -e $essid -c 36 -a "$mac" $device &
-                echo "Starting $location-$i"
-                echo "SSID:$essid... "
-                echo "MAC:$mac"
-        done
->>>>>>> 81cd71e93b6cd5449ac648231997dc103a1b9388:airbase-5g-spec.sh
 
 }
 
 if [ $UID -ne 0 ];then
-<<<<<<< HEAD:_airbase-5g.sh
 	echo "This Script Must Run As Root..."
 	exit
 fi
@@ -84,37 +61,3 @@ sleep 1
 
 
 #[-W 1 (for wep)/ -W 1 -z 2 (for wpa)/ -W 1 -Z 2 (for wpa2)]
-=======
-        echo "This Script Must Run As Root..."
-        exit
-fi
-
-pkill airbase-ng
-
-if [ "$1" = "" ];then
-        echo "no input file"
-        exit
-elif [ ! -e $1 ];then
-        echo "no such file"
-        exit
-elif [ ! -f $1 ];then
-        echo "invalid file format"
-        exit
-fi
-
-if [ "$2" = "" ];then
-        echo "no device input"
-#       device="wlan0"
-#       echo "try using $device.."
-        exit
-else
-        ifconfig  -a |grep $device
-        if [ $? != 0 ];then
-                echo "device not found.."
-                exit
-        fi
-fi
-
-spoof
-sleep 1
->>>>>>> 81cd71e93b6cd5449ac648231997dc103a1b9388:airbase-5g-spec.sh
